3b6b4974dc9174e243591173901fc3f45c1c7db5
Abaljerind <abaljerinda@gmail.com>
2024-04-24 17:13:08 +0700

    create function makeTodo()

--- js/script.js
+++ js/script.js
@@ -47,4 +47,24 @@ document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener(RENDER_EVENT, function () {
     console.log(todos);
   });
+
+  //   function makeTodo untuk menghasilkan sebuah item untuk mengisi container todos
+  function makeTodo(todoObject) {
+    const textTitle = document.createElement("h2");
+    textTitle.innerText = todoObject.task;
+
+    const textTimestamp = document.createElement("p");
+    textTimestamp.innerText = todoObject.timestamp;
+
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("inner");
+    textContainer.append(textTitle, textTimestamp);
+
+    const container = document.createElement("div");
+    container.classList.add("item", "shadow");
+    container.append(textContainer);
+    container.setAttribute("id", `todo-${todoObject.id}`);
+
+    return container;
+  }
 });
