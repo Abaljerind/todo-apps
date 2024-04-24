9afc7161cc3da62cce605d0c6fa8e55f9662ef1d
Abaljerind <abaljerinda@gmail.com>
2024-04-24 14:21:21 +0700

    adding events

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// menambahkan event pada halaman document ketika halaman web selesai dimuat / ditampilkan oleh browser.
+document.addEventListener("DOMContentLoaded", function () {
+  const todos = [];
+  const RENDER_EVENT = "render-todo";
+
+  // menambahkan event pada #form berupa 'submit' agar mencegah di refresh halaman websitenya & memanggil function addTodo();
+  const submitForm = document.getElementById("form");
+  submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addTodo();
+  });
+
+  //   function addTodo()
+  function addTodo() {
+    const textTodo = document.getElementById("title").value;
+    const timestamp = document.getElementById("date").value;
+
+    const generateID = generateId();
+    const todoObject = generateTodoObject(
+      generateID,
+      textTodo,
+      timestamp,
+      false
+    );
+    todos.push(todoObject);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  //   function generateId()
+  function generateId() {
+    return +new Date();
+  }
+
+  // function generateTodoObject()
+  function generateTodoObject(id, task, timestamp, isCompleted) {
+    return {
+      id,
+      task,
+      timestamp,
+      isCompleted,
+    };
+  }
+
+  //   membuat listener dari RENDER_EVENT dengan menampilkan array todos menggunakan console.log();
+  document.addEventListener(RENDER_EVENT, function () {
+    console.log(todos);
+  });
+});
